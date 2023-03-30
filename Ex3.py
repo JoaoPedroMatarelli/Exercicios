38fa054da2efeba7cd49fb06035e478befa1fb65
JoaoPedroMatarelli <82184391+JoaoPedroMatarelli@users.noreply.github.com>
2023-03-30 09:40:06 -0300

    Create Ex3.py

---
+++ Ex3.py
@@ -0,0 +1,17 @@
+"""
+3) Descubra a lógica e complete o próximo elemento:
+
+
+
+a) 1, 3, 5, 7, 9 # Sempre soma 2
+
+b) 2, 4, 8, 16, 32, 64, # Sempre multiplica por 2
+
+c) 0, 1, 4, 9, 16, 25, 36, 49 Os numero são cresente e são multipicados por eles mesmos 5.5 = 25   6.6. = 36    7.7 = 49
+
+d) 4, 16, 36, 64, 100.  # Sempre adiciona 8 no murero que vai somar o procimo numero. O primeiro numero a que somou o 4 foi 12
+
+e) 1, 1, 2, 3, 5, 8, 13   #fibonacci soma sempre os dois ultimos numeros
+
+f) 2,10, 12, 16, 17, 18, 19, ____
+"""
