3515670eb40f9440faf6512bac2f16a67d446abe
JoaoPedroMatarelli <82184391+JoaoPedroMatarelli@users.noreply.github.com>
2023-03-30 09:39:00 -0300

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -1,3 +1,25 @@
+"""
+1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+
+K = K + 1;
+
+SOMA = SOMA + K;
+
+}
+
+imprimir(SOMA);
+
+
+
+Ao final do processamento, qual será o valor da variável SOMA?
+"""
+
 INDICE = 13
 SOMA = 0
 K = 0
@@ -6,5 +28,7 @@ while K < INDICE:
     K = K + 1
     SOMA = SOMA + K
     
-#O print era mostra o valor 91
+#O print ira mostra o valor 91
 print(SOMA)
+
+
